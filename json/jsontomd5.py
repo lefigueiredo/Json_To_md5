4649e6964479e88c9522ef852bd352014ae82f86
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2020-06-10 13:26:31 -0300

    Criando DICT de Network

--- json/jsontomd5.py
+++ json/jsontomd5.py
@@ -3,12 +3,11 @@ import hashlib
 
 
 def ler_json(file_name: str):
-    '''
+    """
     Ler json e armazenar na memoria
     :param file_name: Nome do arquivo json
     :return: Retorna um dict com os dados do json
-    '''
-    data = {}
+    """
     with open(file_name+'.json', 'r') as f:
         data = json.load(f)
         f.close()
@@ -16,18 +15,17 @@ def ler_json(file_name: str):
 
 
 def cria_dict(linha: str):
-    dicionario = {}
-    dicionario['ID'] = linha[linha.find("conn")+4:linha.find("]")]
-    dicionario['DATA'] = linha[:10]
-    dicionario['HORA'] = linha[11:19]
-    dicionario['BANCO'] = ""
-    dicionario['IP'] = ""
-    dicionario['AMDHM'] = linha[:16].replace("-","").replace(":","").replace("T","")
+    dicionario = {
+        'ID': linha[linha.find("conn")+4: linha.find("]")],
+        'DATA': linha[:10],
+        'HORA': linha[11:19],
+        'BANCO': "",
+        'IP': "",
+        'AMDHM': linha[:16].replace("-", "").replace(":", "").replace("T", "")
+    }
 
     if "metadata from" in linha:
-        dicionario['IP'] = linha[linha.find("metadata from")+14:linha.find("]")]
-
-
+        dicionario['IP'] = linha[linha.find("metadata from")+14: linha.find(":")]
 
     # Tratamento dos itens da lista de acessos
     if dicionario['ID'] != "":
@@ -37,16 +35,14 @@ def cria_dict(linha: str):
         del dicionario
 
 
-
 lista_md5 = []
 lista_acessos = []
 dados_json = ler_json('log')
 
-
 for chave, valor in dados_json.items():
     if chave == 'log':
         while True:
-            for i in range(300): # MUDA AQUIIII PRA TESTARRRR MULHEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR <------------
+            for i in range(50): # MUDA AQUIIII PRA TESTARRRR MULHEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR <------------
                 string_md5 = (hashlib.md5(valor[i].encode())).hexdigest()
                 '''Cria o md5 de todas as linha do JSON. ".encode()" cria o 
                 md5 e o "hexdigest" converte para string hexadecimal'''
@@ -56,7 +52,7 @@ for chave, valor in dados_json.items():
                     lista_md5.append(string_md5)
                     if "NETWORK" in valor[i]:
                         dict_network = cria_dict(valor[i])
-                        if dict_network!= None:
+                        if dict_network is not None:
                             lista_acessos.append(cria_dict(valor[i]))
             break
 
