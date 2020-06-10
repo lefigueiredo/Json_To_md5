91bd943a8c8c9a34cbfcc014cca8529bca3aae0a
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2020-06-10 13:00:13 -0300

    Criando DICT de Network

--- json/jsontomd5.py
+++ json/jsontomd5.py
@@ -15,14 +15,37 @@ def ler_json(file_name: str):
         return data
 
 
+def cria_dict(linha: str):
+    dicionario = {}
+    dicionario['ID'] = linha[linha.find("conn")+4:linha.find("]")]
+    dicionario['DATA'] = linha[:10]
+    dicionario['HORA'] = linha[11:19]
+    dicionario['BANCO'] = ""
+    dicionario['IP'] = ""
+    dicionario['AMDHM'] = linha[:16].replace("-","").replace(":","").replace("T","")
+
+
+
+
+
+
+    if dicionario['ID'] != "":
+        # Se ID = Nulo, significa que NETWORK não possuí [conn#####]
+        return dicionario
+    else:
+        del dicionario
+
+
+
 lista_md5 = []
+lista_acessos = []
 dados_json = ler_json('log')
 
 
 for chave, valor in dados_json.items():
     if chave == 'log':
         while True:
-            for i in range(len(chave)): # MUDA AQUIIII PRA TESTARRRR MULHEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR <------------
+            for i in range(300): # MUDA AQUIIII PRA TESTARRRR MULHEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR <------------
                 string_md5 = (hashlib.md5(valor[i].encode())).hexdigest()
                 '''Cria o md5 de todas as linha do JSON. ".encode()" cria o 
                 md5 e o "hexdigest" converte para string hexadecimal'''
@@ -30,4 +53,13 @@ for chave, valor in dados_json.items():
                     continue
                 else:
                     lista_md5.append(string_md5)
+                    if "NETWORK" in valor[i]:
+                        dict_network = cria_dict(valor[i])
+                        if dict_network!= None:
+                            lista_acessos.append(cria_dict(valor[i]))
             break
+
+
+print(lista_md5)
+for i in lista_acessos:
+    print(i, end="\n")
